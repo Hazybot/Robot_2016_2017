e423276b35358a6b87bd8421ab2cb66b64b88a6d
MattVil <mattvilain@gmail.com>
2017-05-25 11:12:23 +0200

    code pour le capteur ultrason avant

--- arduino/brasBalles/testServo/testServo.ino
+++ arduino/brasBalles/testServo/testServo.ino
@@ -1,11 +1,15 @@
-
 #include <Servo.h>
 
+/*position pour la pince*/
 #define POS_OUVERTE 35
 #define POS_FERME 180
 
-#define POS_ATTRAPE 10
+
+/*position pour le bras*/
+#define POS_ATTRAPE 15
+#define POS_MID 90
 #define POS_STOCK 180
+
 Servo pince;
 Servo bras;
 
@@ -14,11 +18,20 @@ int flag = 1;
 void setup(){
    pince.attach(6);
    bras.attach(5);
-
+   Œ
+   bras.write(POS_STOCK);
+   pince.write(POS_FERME);
+  
 }
 
 void loop(){
-  while(flag){
+  moveToGrap();
+}
+
+
+void moveToGrap(){
+    bras.write(POS_MID);
+    delay(1000);
     pince.write(POS_OUVERTE);
     delay(1000);
     bras.write(POS_ATTRAPE);
@@ -26,10 +39,11 @@ void loop(){
     pince.write(POS_FERME);
     delay(1000);
     bras.write(POS_STOCK);
+    delay(1500);
+    pince.write(POS_OUVERTE);
     delay(1000);
-    bras.write(POS_OUVERTE);
+    bras.write(POS_STOCK);
     delay(1000);
-    flag = 1;
-  }
+    pince.write(POS_FERME);
+    delay(1000);
 }
-
